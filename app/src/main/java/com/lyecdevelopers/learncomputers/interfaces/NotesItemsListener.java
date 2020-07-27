5c15dd5670e3e3547ff03485297e3f81634d4ff3
jaba <jabajnan8@gmail.com>
2020-07-27 13:14:45 +0000

    Added RecyclerView

---
+++ app/src/main/java/com/lyecdevelopers/learncomputers/interfaces/NotesItemsListener.java
@@ -0,0 +1,7 @@
+package com.lyecdevelopers.learncomputers.interfaces;
+
+import com.lyecdevelopers.learncomputers.models.Notes;
+
+public interface NotesItemsListener {
+    void onClick(Notes notes);
+}
