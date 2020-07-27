e4e81a958420f7b4f2a2d37ff93343dc4f296c03
jaba <jabajnan8@gmail.com>
2020-07-27 12:15:06 +0000

    Added RecyclerView

--- app/src/main/java/com/lyecdevelopers/learncomputers/MainActivity.java
+++ app/src/main/java/com/lyecdevelopers/learncomputers/MainActivity.java
@@ -1,14 +1,35 @@
 package com.lyecdevelopers.learncomputers;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lyecdevelopers.learncomputers.adapters.NotesAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    RecyclerView mRecyclerView;
+    NotesAdapter mNotesAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        initRecycler();
+
+    }
+
+    private void initRecycler() {
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mNotesAdapter);
     }
 }
--- app/src/main/java/com/lyecdevelopers/learncomputers/adapters/NotesAdapter.java
+++ app/src/main/java/com/lyecdevelopers/learncomputers/adapters/NotesAdapter.java
@@ -1,26 +1,57 @@
 package com.lyecdevelopers.learncomputers.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.lyecdevelopers.learncomputers._base.BaseViewHolder;
+import com.lyecdevelopers.learncomputers.R;
+import com.lyecdevelopers.learncomputers.models.Notes;
 
-public class NotesAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesAdapterViewHolder> {
+
+
+    List<Notes> notesList;
+
+    public NotesAdapter(List<Notes> notesList) {
+        this.notesList = notesList;
+    }
+
     @NonNull
     @Override
-    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NotesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_notes_items, parent, false);
+        return new NotesAdapterViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NotesAdapterViewHolder holder, int position) {
+        Notes notes = notesList.get(position);
+        holder.mTitle.setText(notes.getTitle());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notesList.size();
+    }
+
+    public class NotesAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTitle;
+        TextView mDescription;
+
+        public NotesAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTitle = itemView.findViewById(R.id.txttitle);
+
+        }
     }
 }
