ea94c7753df5676143edaee399323b2fc549d52e
jaba <jabajnan8@gmail.com>
2020-07-27 15:00:39 +0000

    Added RecyclerView

--- app/src/main/java/com/lyecdevelopers/learncomputers/activities/SingleNotesDetail.java
+++ app/src/main/java/com/lyecdevelopers/learncomputers/activities/SingleNotesDetail.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.lyecdevelopers.learncomputers.R;
 
@@ -12,20 +13,37 @@ public class SingleNotesDetail extends AppCompatActivity {
 
     TextView mTitle;
     TextView mDescripiton;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_details);
 
-        mTitle = findViewById(R.id.txtTitle);
-        mDescripiton = findViewById(R.id.txtDescription);
+        mTitle = findViewById(R.id.notes_title);
+        mDescripiton = findViewById(R.id.notes_description);
+        toolbar = findViewById(R.id.toolbar);
 
+
+        if (getSupportActionBar() == null) {
+            setSupportActionBar(toolbar);
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setTitle("Details");
+        }
+
+
+        setData();
+
+
+    }
+
+    private void setData() {
         String title = getIntent().getStringExtra("title");
         String description = getIntent().getStringExtra("description");
-
         mTitle.setText(title);
         mDescripiton.setText(description);
-
     }
 }
